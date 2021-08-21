922d56fc4a4de99d3df8b47f730c8266485cecb5
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-08-22 01:17:55 +0530

    Extract the data from CSV files

--- src/server/index.js
+++ src/server/index.js
@@ -2,3 +2,14 @@
 
 const fs = require('fs'); // File System module import
 const Papa = require('papaparse'); // Papa-Parse module import
+
+
+// Extract data:
+
+const matchesFileData = fs.readFileSync(__dirname + '/../data/matches.csv', 'utf-8') // Read matches.csv into string
+const deliveriesFileData = fs.readFileSync(__dirname + '/../data/deliveries.csv', 'utf-8') // Read deliveries.csv into string
+
+const csvConfiguration = { // Configuration object for Papa-Parse
+	header: true,
+	dynamicTyping: true
+}
