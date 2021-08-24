6f9e33ddc19c8add1659dd097dbec46cfb17f88c
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-08-24 13:35:29 +0530

    Make a function to calculate extra runs per team in 2016

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -42,39 +42,19 @@ const exportFunctions = {
     // 3. Calculate the extra runs conceded per team in the year 2016.
 
     calculateExtrasPerTeam (deliveries, matches, targetYear = 2016) {
-        const targetYearIds = [];
-
-        for(match of matches) { // Collect the match ID for targetYear
-            if(match.season === targetYear && !(match.season in targetYearIds)) {
-                targetYearIds.push(match.id)
-            }
-        }
-
         const extrasPerTeam = {};
-
+        
         for(delivery of deliveries) {
-            if(delivery.match_id in targetYearIds) {
-                if(delivery.bowling_team in extrasPerTeam) {
-                    extrasPerTeam[delivery.bowling_team] += delivery.extra_runs;
-                } else {
-                    extrasPerTeam[delivery.bowling_team] = delivery.extra_runs;
+            for(match of matches) {
+                if(delivery.match_id === match.id && match.season === targetYear) {
+                    if(delivery.bowling_team in extrasPerTeam) {
+                        extrasPerTeam[delivery.bowling_team] += delivery.extra_runs;
+                    } else {
+                        extrasPerTeam[delivery.bowling_team] = delivery.extra_runs;
+                    }
                 }
             }
         }
-
-        // const extrasPerTeam = {};
-        
-        // for(delivery of deliveries) {
-        //     for(match of matches) {
-        //         if(delivery.match_id === match.id && match.season === targetYear) {
-        //             if(delivery.bowling_team in extrasPerTeam) {
-        //                 extrasPerTeam[delivery.bowling_team] += delivery.extra_runs;
-        //             } else {
-        //                 extrasPerTeam[delivery.bowling_team] = delivery.extra_runs;
-        //             }
-        //         }
-        //     }
-        // }
 
         return extrasPerTeam;
     }
